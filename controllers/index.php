58f22503fc250d00ee83137085b573e025d63aac
Phelipe Henrique Fernandes Vieira <felihenrique@imd.local>
2018-06-08 15:42:49 -0300

    Adicionado link para vagas

--- controllers/index.php
+++ controllers/index.php
@@ -2,7 +2,9 @@
     require_once("autenticacao.php");
     require_once('UsuarioRepository.php');
     $userRepo = new UsuarioRepository();
-    $usuario = $userRepo->buscarPorId($_SESSION['idusuario']);
+	if(isset($_SESSION['idusuario'])) {
+		$usuario = $userRepo->buscarPorId($_SESSION['idusuario']);
+	}
     render_view("inicio.php", [
         'usuario' => $usuario
     ]);
