6c8391b73075be660572e8f91c9eb36441045c36
Phelipe Henrique Fernandes Vieira <felihenrique@imd.local>
2018-06-08 15:42:49 -0300

    Adicionado link para vagas

--- views/header.php
+++ views/header.php
@@ -17,11 +17,19 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="#">
-        <i class="fas fa-briefcase"></i>
-        Vagas
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-pencil-alt"></i>
+          Vagas
         </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/vagas/criarVaga.php">
+          Criar
+          </a>
+          <a class="dropdown-item" href="/vagas/listarVagas.php">
+          Listar
+          </a>
+        </div>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
