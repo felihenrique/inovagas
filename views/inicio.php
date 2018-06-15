997330af3dbe7fa449c3251f74e06dee9eb46f46
Jessiely Oliveira <jessiely@imd.ufrn.br>
2018-06-14 22:35:25 -0300

    funcionalidades por perfil

--- views/inicio.php
+++ views/inicio.php
@@ -4,6 +4,7 @@
 <?php if ($perfil == "administrador") { ?>
 		<ul>
 			<li><a href="/usuario/aprovacaoempresa.php">Empresas pré cadastradas</a></li>
+
 		</ul>
 <?php } ?>
 
@@ -11,12 +12,13 @@
 		<ul>
 			<li><a href="/vagas/criarVaga.php">Criar vaga</a></li>
 			<li><a href="">Editar vaga</a></li>
+			<li><a href="">Vagas criadas</a></li>
 		</ul>
 <?php } ?>
 
 <?php if ($perfil == "aluno") { ?>
 		<ul>
-			<li><a href="">Empresas pré cadastradas</a></li>
+			<li><a href="">Inscrições</a></li>
 		</ul>
 <?php } ?>
 
